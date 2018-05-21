29db6e63dc886f24620063016a03822c87cfee54
Giovanni Sakti <giosakti@gmail.com>
2018-05-21 15:58:44 +0700

    [gio/tara] don't continue if hosts are empty

--- recipes/search.rb
+++ recipes/search.rb
@@ -4,6 +4,9 @@
 #
 # Copyright:: 2018, BaritoLog.
 
+# Don't continue if these variables are empty
+return if node[cookbook_name]['hosts'].empty?
+
 # Keep it simple for now
 node.run_state[cookbook_name] = {}
 node.run_state[cookbook_name]['hosts'] = node[cookbook_name]['hosts']
