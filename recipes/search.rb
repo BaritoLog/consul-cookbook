f193b2fc3a6792bb6cbae82bb059a96a5a78329b
Giovanni Sakti <giosakti@gmail.com>
2018-05-21 16:30:55 +0700

    [gio/tara] don't reinitialize variable if it's already exist

--- recipes/search.rb
+++ recipes/search.rb
@@ -8,5 +8,5 @@
 return if node[cookbook_name]['hosts'].empty?
 
 # Keep it simple for now
-node.run_state[cookbook_name] = {}
+node.run_state[cookbook_name] ||= {}
 node.run_state[cookbook_name]['hosts'] = node[cookbook_name]['hosts']
