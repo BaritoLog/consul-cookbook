3221b9d07317a64783ab203ca3a0eaac66c406e0
opan <opan.neutron@gmail.com>
2018-05-28 15:22:37 +0700

    Update fix custom resource register service

--- resources/register_service.rb
+++ resources/register_service.rb
@@ -1,24 +1,25 @@
-property :configs, Hash, default: {}, required: true
-property :config_dir, String, required: true
-property :exec_file, String, required: true
-property :user, String, required: true
-property :group, String, required: true
+property :configs, 	Array, default: [], required: true
+property :config_dir, 	String, required: true
+property :exec_file, 	String, required: true
+property :user, 	String, required: true
+property :group, 	String, required: true
 
 action :run do
-  configs.each do |config|
+  new_resource.configs.each do |config|
     name = config['name']
+
     # Create service configuration file
     config = { "service" => config }
-    file "#{config_dir}/#{name}.json" do
+    file "#{new_resource.config_dir}/#{new_resource.name}.json" do
       content Chef::JSONCompat.to_json_pretty(config)
-      owner user
-      group group
+      owner new_resource.user
+      group new_resource.group
       mode '0640'
     end
   end
 
   # Reload Consul configuration
   execute "reload config" do
-    command "#{exec_file} reload"
+    command "#{new_resource.exec_file} reload"
   end
 end
