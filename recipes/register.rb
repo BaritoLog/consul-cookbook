7f672a0b002da548c6184c25e8a4fcb04c79acbd
opan <opan.neutron@gmail.com>
2018-05-28 09:45:09 +0700

    Fix type recipe register

--- recipes/register.rb
+++ recipes/register.rb
@@ -8,19 +8,23 @@
 return if node.run_state.dig(cookbook_name, 'hosts').nil?
 
 node[cookbook_name]['registered_services'].each do |srv|
-  next if service['name'].nil?
-  service = srv.to_hash
-  file "#{node[cookbook_name]['config_dir']}/#{service['name']}.json" do
-    content Chef::JSONCompat.to_json_pretty(service)
-    owner node[cookbook_name]['user']
-    group node[cookbook_name]['group']
-    mode '0640'
+  # Don't register service if attribute name is nil
+  unless srv['name'].nil?
+    service = { "service" => srv.to_hash }
+    file "#{node[cookbook_name]['config_dir']}/#{srv['name']}.json" do
+      content Chef::JSONCompat.to_json_pretty(service)
+      owner node[cookbook_name]['user']
+      group node[cookbook_name]['group']
+      mode '0640'
+    end
   end
 end
 
 bin = "#{node[cookbook_name]['prefix_home']}/bin/consul"
 
-# Reload Consul configurations
-execute "reload config" do
-  command "#{bin} reload"
+if node[cookbook_name]['registered_services'].length > 0
+  # Reload Consul configurations
+  execute "reload config" do
+    command "#{bin} reload"
+  end
 end
