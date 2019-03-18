06f3121fe747dd08720bfb7b5db4821bd598ba59
Giovanni Sakti <giosakti@gmail.com>
2019-03-18 17:50:29 +0700

    [gio/puja] add step to build the base

--- scripts/gitlabci-install-lxd.sh
+++ scripts/gitlabci-install-lxd.sh
@@ -1,7 +1,6 @@
 set -x
 set -e
 sudo apt update
-sudo apt install ca-certificates -yqq
 sudo apt install lxd lxd-client lxd-tools -yqq
 sudo service lxd start
-sudo lxc storage create default dir source=/var/lib/lxd/storage-pools/default
+cat ./scripts/lxd-preseed.yml | sudo lxd init --preseed
