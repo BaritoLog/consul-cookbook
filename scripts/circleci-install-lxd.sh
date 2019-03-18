f9cb06e60ac39abc8184cc5b92df02296c605808
Giovanni Sakti <giosakti@gmail.com>
2019-03-18 15:32:32 +0700

    [gio/puja] install lxd directly from apt

--- scripts/circleci-install-lxd.sh
+++ scripts/circleci-install-lxd.sh
@@ -1,9 +1,4 @@
 set -x
 set -e
 apt update
-apt purge lxd* -yqq
-apt install snapd -yqq
-systemctl enable snapd.service
-systemctl start snapd.service
-sleep 5
-snap install lxd --channel=3.0/stable
+apt install lxd lxd-client lxd-tools -yqq
