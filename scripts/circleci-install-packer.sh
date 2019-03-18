edd7244a851a68bb8d910d7eda3a75f6d1aed9b4
Giovanni Sakti <giosakti@gmail.com>
2019-03-18 15:10:14 +0700

    [gio/puja] also install unzip

--- scripts/circleci-install-packer.sh
+++ scripts/circleci-install-packer.sh
@@ -2,7 +2,7 @@ set -x
 set -e
 if [ ! -e packer/packer ]; then
   apt update
-  apt install wget -yqq
+  apt install wget unzip -yqq
   wget https://releases.hashicorp.com/packer/1.3.5/packer_1.3.5_linux_amd64.zip
   unzip packer_1.3.5_linux_amd64.zip -d packer
 fi
